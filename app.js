e6de4f8e289334c5fe7a9f0a4d1473ab729630fe
Dauda Yusuf <jenomy32@gmail.com>
2022-07-04 23:22:20 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+const port = 5000;
+
+app.get('/', (req, res)=>{
+    res.render('index')
+})
+app.get('/about', (req, res)=>{
+    res.render('about')
+})
+app.get('/contact', (req, res)=>{
+    res.render('contact')
+})
+
+app.listen(process.env.PORT || port, ()=>{
+    console.log(`server is running at ${port}`);
+})
